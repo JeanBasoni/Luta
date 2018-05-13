2fd56cf517582fede4eaf2ea233a51fa3f9a1ab1
JeanBasoni <JeanBasoni@gmail.com>
2018-05-12 22:14:06 -0300

    JeanBasoni *Area admin implementada

--- JBS.UI/JBS.UI/App_Start/RouteConfig.cs
+++ JBS.UI/JBS.UI/App_Start/RouteConfig.cs
@@ -14,9 +14,16 @@ namespace JBS.UI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new
+            {
+                controller = "Home",
+                action = "Index",
+                id = UrlParameter.Optional
+            }
+            // caso use Areas defina o namespace
+            , namespaces: new[] { "JBS.UI.Controllers" }
             );
         }
     }
